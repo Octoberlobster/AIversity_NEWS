177416762c39e1a3e38a20bdb0df9ff3232f44af
how1688 <how1688@gmail.com>
2025-06-17 13:57:56 +0800

    123

--- schedule_test.py
+++ schedule_test.py
@@ -7,8 +7,6 @@ def run_a():
     subprocess.Popen(["python", "cr_with_supabase2.py"])
     time.sleep(120)  # 等待 1 秒，確保 a.py 完成
     subprocess.Popen(["python", "clean_daa.py"])
-    time.sleep(900)  # 等待 900 秒，確保 a.py 完成
-    subprocess.Popen(["python", "SE_test2.py"])
 
 # 設定排程
 # schedule.every(60).minutes.do(run_a)   # 每1分鐘跑一次 b.py
