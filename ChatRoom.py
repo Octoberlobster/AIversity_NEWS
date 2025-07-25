0eb6010c92cc0f6ac2e87f424163eb1a98c06f1c
Tiger0124 <dogs247841@gmail.com>
2025-07-25 10:06:56 +0800

    聊天室--不知道怎麼加到前端

--- ChatRoom.py
+++ ChatRoom.py
@@ -1,55 +1,157 @@
-import google.generativeai as genai
-import json
-from time import sleep
+from google import genai
+from google.genai import types
+from supabase import create_client, Client
 import os
-import asyncio
 
-# 設定 API 金鑰
+# 設定環境變數
 api_key = os.getenv("API_KEY_Gemini")
-genai.configure(api_key=api_key)
-with open("GenerateNews_EachEvent\\News.json", "r", encoding="utf-8") as f:
-    content = f.read()
-    content = json.loads(content)
-content = content["Content"]
+gemini_client = genai.Client(api_key=api_key)
+api_key_supabase = os.getenv("API_KEY_Supabase")
+supabase_url = os.getenv("Supabase_URL")
+supabase: Client = create_client(supabase_url, api_key_supabase)
 
-system_instruction = content+"你是在此新聞中的戰爭平民，請你以簡短的幾句話來進行對話式的回答，並請你扮演以下角色，這是他的立場與看法"\
-                    ":我們每天都生活在恐懼和不確定之中。戰爭摧毀了我們的生活，奪走了我們的家園和親人。我們渴望和平，希望戰爭儘快結束，讓我們能夠重建我們的生活。我們希望國際社會能夠聽到我們的聲音，並採取行動幫助我們擺脫困境。我們只是想過上正常的生活，讓孩子們能夠在和平的環境中成長。"
+model_flag = ["Politics","Science","Technology"] # 改前端的選擇結果
 
-system_instruction2 = content+"你是在此新聞中的戰爭平民，請你以簡短的幾句話來進行對話式的回答，並請你扮演以下角色，這是他的立場與看法"\
-                    ":我們渴望和平，但也絕不接受任何損害我們主權和領土完整的協議。我們感謝美國的援助，但我們需要更強而有力的安全保障。美俄之間的單獨談判令我們感到擔憂，我們擔心我們的利益會在沒有我們充分參與的情況下被犧牲。我們願意談判，但前提是俄羅斯必須尊重我們的獨立和自決權。我們的人民為自由付出了巨大的代價，我們不會放棄為我們的國家而戰。"
+model_dict = {
+    "Politics": -1,
+    "Social News": -1,
+    "Science": -1,
+    "Technology": -1,
+    "International News": -1,
+    "Lifestyle & Consumer News": -1,
+    "Sports": -1,
+    "Entertainment": -1,
+    "Business & Finance": -1,
+    "Health & Wellness": -1,
+}
 
-print("Instruction:", system_instruction)
-print("Instruction2:", system_instruction2)
+def create_model(category):
+    # 要設定json格式的話再參考https://ai.google.dev/gemini-api/docs/migrate?hl=zh-tw
 
-# 建立對話模型
-model = genai.GenerativeModel('gemini-1.5-pro-002', system_instruction=system_instruction)
+    # 這部份確定再加
+    # like_RAG_data = (
+    #     supabase.table("generated_news")
+    #     .select("content")
+    #     .eq("category", category)
+    #     .execute()
+    # )
 
-model2 =genai.GenerativeModel('gemini-1.5-pro-002', system_instruction=system_instruction2)
+    model = "gemini-2.0-flash"
 
+    if category == "Politics":
+        system_instruction = "你是政治專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Social News":
+        system_instruction = "你是社會新聞專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Science":
+        system_instruction = "你是科學專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Technology":
+        system_instruction = "你是科技專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "International News":
+        system_instruction = "你是國際新聞專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Lifestyle & Consumer News":
+        system_instruction = "你是生活與消費新聞專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Sports":
+        system_instruction = "你是體育專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Entertainment":
+        system_instruction = "你是娛樂專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Business & Finance":
+        system_instruction = "你是商業與金融專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    elif category == "Health & Wellness":
+        system_instruction = "你是健康與福祉專家"
+        return gemini_client.chats.create(
+            model=model,
+            config=types.GenerateContentConfig(
+                system_instruction=system_instruction
+                #response_mime_type="application/json",
+                #response_json_schema=
+            ),
+        )
+    else:
+        raise ValueError("Unknown category: {}".format(category))
 
-# 建立對話歷史 (讓模型記住對話)
-chat_session = model.start_chat()
-chat_session2 = model2.start_chat()
+def chat(prompt, category):
+    if model_dict[category] == -1:
+        model_dict[category] = create_model(category)
+    
+    response = model_dict[category].send_message(message=prompt)
+    
+    return response
 
-async def get_user_input():
-    """非同步讀取使用者輸入"""
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, input, "請輸入你的訊息 (輸入 'exit' 離開)：\n")
+# prompt = "你好"
+# for category in model_flag:
+#     print(f"Category: {category}")
+#     print(chat(prompt, category).text)
+#     print("-" * 40)
 
-async def chat():
-    """非同步對話處理"""
-    while True:
-        message = await get_user_input()
-        if message.lower() == "exit":
-            break
-        
-        response = await asyncio.to_thread(chat_session.send_message, message)
-        print("Model Response:", response.text)
-        sleep(0.5)
-        response2 = await asyncio.to_thread(chat_session2.send_message, message)
-        print("Model2 Response:", response2.text)
-        
-        await asyncio.sleep(5)  # 非同步 sleep，不會阻塞主執行緒
-
-if __name__ == "__main__":
-    asyncio.run(chat())
